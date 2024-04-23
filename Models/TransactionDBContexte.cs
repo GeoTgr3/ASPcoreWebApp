3c1bbfa00ccf86892ba6a7e01e9abca898e381ec
GeoTgr3 <achraf6bel@gmail.com>
2024-04-23 15:15:19 +0100

    Add project files.

---
+++ Models/TransactionDBContexte.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ASPcoreWebApp.Models
+{
+    public class TransactionDbContext : DbContext
+    {
+        public TransactionDbContext(DbContextOptions<TransactionDbContext> options) : base(options)
+        { }
+
+        public DbSet<Transactions> Transactions { get; set; }
+    }
+}
